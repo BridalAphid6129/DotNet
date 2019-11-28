e49d6e52e422452ece9cf4fe15440201f1bbd58d
Sven Höpfner <sven.hoepfner99@gmail.com>
2019-11-28 21:07:59 +0100

    create basic entities to use them in the dto converter

--- AutoReservation.Dal/Entities/Auto.cs
+++ AutoReservation.Dal/Entities/Auto.cs
@@ -1,4 +1,10 @@
 namespace AutoReservation.Dal.Entities
 {
-
+    public abstract class Auto
+    {
+        public int Id { get; set; }
+        public string Marke { get; set; }
+        public byte[] RowVersion { get; set; }
+        public int Tagestarif { get; set; }
+    }
 }
--- AutoReservation.Dal/Entities/Reservation.cs
+++ AutoReservation.Dal/Entities/Reservation.cs
@@ -1,4 +1,14 @@
+using System;
+
 namespace AutoReservation.Dal.Entities
 {
-
+    public class Reservation
+    {
+        public int AutoId { get; set; }
+        public DateTime Bis { get; set; }
+        public int KundeId { get; set; }
+        public int ReservationsNr { get; set; }
+        public byte[] RowVersion { get; set; }
+        public DateTime Von { get; set; }
+    }
 }
