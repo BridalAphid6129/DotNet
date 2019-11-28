73f28caa1fe40134984e01ad10ff033b2622351e
Sven Höpfner <sven.hoepfner99@gmail.com>
2019-11-28 21:09:18 +0100

    server in now able to start on mac

--- AutoReservation.Service.Grpc/Program.cs
+++ AutoReservation.Service.Grpc/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 
 namespace AutoReservation.Service.Grpc
@@ -17,7 +18,14 @@ namespace AutoReservation.Service.Grpc
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        // Setup a HTTP/2 endpoint without TLS.
+                        options.ListenLocalhost(5000, o => o.Protocols = 
+                            HttpProtocols.Http2);
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
 }
