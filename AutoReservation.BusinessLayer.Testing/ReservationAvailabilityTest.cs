d4966de4ca196d43119bd86d01b6aa1e9549a327
lukas <lukas.volk2014@gmail.com>
2019-12-16 21:53:34 +0100

    Tests suceeded!

--- AutoReservation.BusinessLayer.Testing/ReservationAvailabilityTest.cs
+++ AutoReservation.BusinessLayer.Testing/ReservationAvailabilityTest.cs
@@ -64,7 +64,24 @@ namespace AutoReservation.BusinessLayer.Testing
         [Fact]
         public async Task ScenarioOkay03Test()
         {
-            throw new NotImplementedException("Test not implemented.");
+            var reservation1 = await _target.GetReservationById(1);
+            var date1von = new DateTime(2020, 1, 5);
+            var date1bis = new DateTime(2020, 5, 13);
+            reservation1.Von = date1von;
+            reservation1.Bis = date1bis;
+            reservation1.AutoId = 1;
+            await _target.UpdateReservation(reservation1);
+            
+            var reservation2 = await _target.GetReservationById(2);
+            var date2von = new DateTime(2019, 11, 5 );
+            var date2bis = new DateTime(2020, 1, 5);
+            reservation2.Von = date2von;
+            reservation2.Bis = date2bis;
+            reservation2.AutoId = 1;
+            
+            async Task Act() => await _target.UpdateReservation(reservation2);
+            
+            Assert.True(Act().IsCompleted);
             // arrange
             //                | ---Date 1--- |
             //| ---Date 2-- - |
@@ -75,12 +92,24 @@ namespace AutoReservation.BusinessLayer.Testing
         [Fact]
         public async Task ScenarioOkay04Test()
         {
-            throw new NotImplementedException("Test not implemented.");
-            // arrange
-            //                | ---Date 1--- |
-            //| ---Date 2--- |
-            // act
-            // assert
+            var reservation1 = await _target.GetReservationById(1);
+            var date1von = new DateTime(2019, 10, 20);
+            var date1bis = new DateTime(2019, 12, 24);
+            reservation1.Von = date1von;
+            reservation1.Bis = date1bis;
+            reservation1.AutoId = 1;
+            await _target.UpdateReservation(reservation1);
+            
+            var reservation2 = await _target.GetReservationById(2);
+            var date2von = new DateTime(2019, 12, 24 );
+            var date2bis = new DateTime(2020, 1, 5);
+            reservation2.Von = date2von;
+            reservation2.Bis = date2bis;
+            reservation2.AutoId = 1;
+            
+            async Task Act() => await _target.UpdateReservation(reservation2);
+            
+            Assert.True(Act().IsCompleted);
         }
 
         [Fact]
@@ -91,18 +120,21 @@ namespace AutoReservation.BusinessLayer.Testing
             var reservation1 = await _target.GetReservationById(1);
             var date1von = new DateTime(2019, 10, 20);
             var date1bis = new DateTime(2019, 12, 24);
+            reservation1.Von = date1von;
+            reservation1.Bis = date1bis;
+            reservation1.AutoId = 1;
+            await _target.UpdateReservation(reservation1);
+            
             var reservation2 = await _target.GetReservationById(2);
             var date2von = new DateTime(2019, 11, 5 );
             var date2bis = new DateTime(2020, 1, 5);
-            reservation1.Von = date1von;
-            reservation1.Bis = date1bis;
             reservation2.Von = date2von;
             reservation2.Bis = date2bis;
+            reservation2.AutoId = 1;
             
-            async Task Act1() => await _target.UpdateReservation(reservation1);
-            async Task Act2() => await _target.UpdateReservation(reservation2);
+            async Task Act() => await _target.UpdateReservation(reservation2);
             
-            await Assert.ThrowsAsync<AutoUnavailableException>(Act2);
+            await Assert.ThrowsAsync<AutoUnavailableException>(Act);
         }
 
         [Fact]
@@ -113,18 +145,21 @@ namespace AutoReservation.BusinessLayer.Testing
             var reservation1 = await _target.GetReservationById(1);
             var date1von = new DateTime(2019, 12, 24); 
             var date1bis = new DateTime(2020, 1, 6);
+            reservation1.Von = date1von;
+            reservation1.Bis = date1bis;
+            reservation1.AutoId = 1;
+            await _target.UpdateReservation(reservation1);
+            
             var reservation2 = await _target.GetReservationById(2);
             var date2von = new DateTime(2019, 12, 2 );
             var date2bis = new DateTime(2020, 12, 30);
-            reservation1.Von = date1von;
-            reservation1.Bis = date1bis;
             reservation2.Von = date2von;
             reservation2.Bis = date2bis;
+            reservation2.AutoId = 1;
             
-            async Task Act1() => await _target.UpdateReservation(reservation1);
-            async Task Act2() => await _target.UpdateReservation(reservation2);
+            async Task Act() => await _target.UpdateReservation(reservation2);
             
-            await Assert.ThrowsAsync<AutoUnavailableException>(Act2);
+            await Assert.ThrowsAsync<AutoUnavailableException>(Act);
         }
 
         [Fact]
@@ -135,18 +170,21 @@ namespace AutoReservation.BusinessLayer.Testing
             var reservation1 = await _target.GetReservationById(1);
             var date1von = new DateTime(2019, 10, 20);
             var date1bis = new DateTime(2019, 10, 27);
+            reservation1.AutoId = 1;
+            reservation1.Von = date1von;
+            reservation1.Bis = date1bis;
+            await _target.UpdateReservation(reservation1);
+            
             var reservation2 = await _target.GetReservationById(2);
             var date2von = new DateTime(2019, 10, 20 );
             var date2bis = new DateTime(2020, 1, 6);
-            reservation1.Von = date1von;
-            reservation1.Bis = date1bis;
+            reservation2.AutoId = 1;
             reservation2.Von = date2von;
             reservation2.Bis = date2bis;
             
-            async Task Act1() => await _target.UpdateReservation(reservation1);
-            async Task Act2() => await _target.UpdateReservation(reservation2);
+            async Task Act() => await _target.UpdateReservation(reservation2);
             
-            await Assert.ThrowsAsync<AutoUnavailableException>(Act2);
+            await Assert.ThrowsAsync<AutoUnavailableException>(Act);
         }
 
         [Fact]
@@ -154,7 +192,7 @@ namespace AutoReservation.BusinessLayer.Testing
         {
             //| --------Date 1-------- |
             //| ---Date 2--- |
-            var reservation1 = await _target.GetReservationById(2);
+            var reservation1 = await _target.GetReservationById(1);
             var date1von = new DateTime(2019, 10, 20);
             var date1bis = new DateTime(2020, 3, 15);
             reservation1.AutoId = 1;
@@ -162,15 +200,15 @@ namespace AutoReservation.BusinessLayer.Testing
             reservation1.Bis = date1bis;
             await _target.UpdateReservation(reservation1);
             
-            var reservation2 = await _target.GetReservationById(3);
+            var reservation2 = await _target.GetReservationById(2);
             var date2von = new DateTime(2019, 10, 20 );
             var date2bis = new DateTime(2019, 12, 28);
             reservation2.AutoId = 1;
             reservation2.Von = date2von;
             reservation2.Bis = date2bis;
-            async Task Act2() => await _target.UpdateReservation(reservation2);
+            async Task Act() => await _target.UpdateReservation(reservation2);
             
-            await Assert.ThrowsAsync<AutoUnavailableException>(Act2);
+            await Assert.ThrowsAsync<AutoUnavailableException>(Act);
         }
 
         [Fact]
@@ -181,16 +219,19 @@ namespace AutoReservation.BusinessLayer.Testing
             var reservation1 = await _target.GetReservationById(1);
             var date1von = new DateTime(2019, 10, 20);
             var date1bis = new DateTime(2019, 12, 24);
-            var reservation2 = await _target.GetReservationById(2);
             reservation1.Von = date1von;
             reservation1.Bis = date1bis;
+            reservation1.AutoId = 1;
+            await _target.UpdateReservation(reservation1);
+                
+            var reservation2 = await _target.GetReservationById(2);
             reservation2.Von = reservation1.Von;
             reservation2.Bis = reservation1.Bis;
+            reservation2.AutoId = 1;
+                
+            async Task Act() => await _target.UpdateReservation(reservation2);
             
-            async Task Act1() => await _target.UpdateReservation(reservation1);
-            async Task Act2() => await _target.UpdateReservation(reservation2);
-            
-            await Assert.ThrowsAsync<AutoUnavailableException>(Act2);
+            await Assert.ThrowsAsync<AutoUnavailableException>(Act);
         }
     }
 }
